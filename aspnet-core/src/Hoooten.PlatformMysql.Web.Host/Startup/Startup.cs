31f81e00c3cdff0738ff55eb5a9b21169c53ef20
熊猫宝宝 <benny1026@live.cn>
2019-01-31 16:43:03 +0800

    Update Startup.cs

--- aspnet-core/src/Hoooten.PlatformMysql.Web.Host/Startup/Startup.cs
+++ aspnet-core/src/Hoooten.PlatformMysql.Web.Host/Startup/Startup.cs
@@ -85,14 +85,15 @@ namespace Hoooten.PlatformMysql.Web.Startup
                 {
                     //App:CorsOrigins in appsettings.json can contain more than one address with splitted by comma.
                     builder
-                        .WithOrigins(
-                            // App:CorsOrigins in appsettings.json can contain more than one address separated by comma.
-                            _appConfiguration["App:CorsOrigins"]
-                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                .Select(o => o.RemovePostFix("/"))
-                                .ToArray()
-                        )
-                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        //.WithOrigins(
+                        //    // App:CorsOrigins in appsettings.json can contain more than one address separated by comma.
+                        //    _appConfiguration["App:CorsOrigins"]
+                        //        .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                        //        .Select(o => o.RemovePostFix("/"))
+                        //        .ToArray()
+                        //)
+                        //.SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
