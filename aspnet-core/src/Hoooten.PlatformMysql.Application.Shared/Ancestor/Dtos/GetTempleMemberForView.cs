ab178eaa1bbe2277ea1da824f65a1ca6663fe49f
benny <benny1026@live.cn>
2019-03-24 11:37:43 +0800

    Add Migration Sexy column

--- aspnet-core/src/Hoooten.PlatformMysql.Application.Shared/Ancestor/Dtos/GetTempleMemberForView.cs
+++ aspnet-core/src/Hoooten.PlatformMysql.Application.Shared/Ancestor/Dtos/GetTempleMemberForView.cs
@@ -14,6 +14,5 @@ namespace Hoooten.PlatformMysql.Ancestor.Dtos
 
         public string Century { get; set; }
 
-
     }
 }
---
+++ aspnet-core/src/Hoooten.PlatformMysql.EntityFrameworkCore/Migrations/20190324033531_Add_Sexy_User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hoooten.PlatformMysql.Migrations
+{
+    public partial class Add_Sexy_User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sexy",
+                table: "AbpUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sexy",
+                table: "AbpUsers");
+        }
+    }
+}
